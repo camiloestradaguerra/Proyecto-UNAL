97ac7362aaa2dff1c78e076b72dbcadf09a55c0d
camiloestradaguerra <70024479+camiloestradaguerra@users.noreply.github.com>
2021-04-11 14:39:15 -0500

    Update union_dataframes.py
    Carga el dataframe con los datos limpios

--- union_dataframes.py
+++ union_dataframes.py
@@ -30,46 +30,22 @@ start = timeit.default_timer()
 ######################################################################
 
 ##########################################################
-####### ORGANIZACIÓN DEL DATAFRAME MERGED NUSE.CSV #######
+####### ORGANIZACION DEL DATAFRAME MERGED_NUSE1.CSV #######
 ##########################################################
 dataframe1['FECHA'] = pd.to_datetime(dataframe1['FECHA'])
-dataframe1_enero_18 = dataframe1[(dataframe1['FECHA'] >= '2018-01-01 00:00:00') & (dataframe1['FECHA'] <= '2018-01-21 23:59:59')].sort_values(["FECHA"])
-
-def first_dig(x):
-    if len(str(x)) == 3:
-        digits = (int)(math.log10(x))
-        x = (int)(x/pow(10, digits))
-    if len(str(x)) == 4:
-        digits = (int)(math.log10(x))-1
-        x = (int)(x/pow(10, digits))
-    return str(x)
-
-def two_last_dig(x):
-    return str(x % 100)
-
+dataframe1_enero_18 = dataframe1[(dataframe1['FECHA'] >= '2018-01-01 00:00:00') & (dataframe1['FECHA'] <= '2018-01-31 23:59:59')].sort_values(["FECHA"])
 dataframe1_enero_18['FECHA'] = dataframe1_enero_18['FECHA'].astype(str)
-#dataframe1_enero_18['Hora'] = dataframe1_enero_18['HORA'].apply(first_dig).astype(str)
-#dataframe1_enero_18['Minutos'] = dataframe1_enero_18['HORA'].apply(two_last_dig).astype(str)
-#dataframe1_enero_18['Segundos'] = str('00')
-
-#for i in range(0, len(dataframe1_enero_18['Hora'])):
-#    if len(dataframe1_enero_18['Hora'].iloc[i]) == 1: 
-#        dataframe1_enero_18['Hora'].iloc[i] = '0'+ dataframe1_enero_18['Hora'].iloc[i]
-#    if len(dataframe1_enero_18['Minutos'].iloc[i]) == 1:
-#        dataframe1_enero_18['Minutos'].iloc[i] = '0'+ dataframe1_enero_18['Minutos'].iloc[i]
-        
-#dataframe1_enero_18['Tiempo'] = dataframe1_enero_18['Hora'] + ':' + dataframe1_enero_18['Minutos'] + ':' + dataframe1_enero_18['Segundos']        
-#dataframe1_enero_18['Fecha'] = pd.to_datetime(dataframe1_enero_18['FECHA']) + pd.to_timedelta(dataframe1_enero_18['Tiempo'])
 dataframe1_enero_18 = dataframe1_enero_18.sort_values(["FECHA"]) 
 data_df1_enero_18 = gpd.GeoDataFrame(dataframe1_enero_18, columns=['LONGITUD', 'LATITUD', 'FECHA','geometry'])
 data_df1_enero_18['cov1'] = " "
 data_df1_enero_18['cov2'] = " "
 data_df1_enero_18['cell'] = " "
 data_df1_enero_18 = data_df1_enero_18.to_crs("EPSG:3857")
+
 for i in range(0, len(data_df1_enero_18)):
     data_df1_enero_18['geometry'].iloc[i] = Point(nys(data_df1_enero_18['geometry'].iloc[i].x, data_df1_enero_18['geometry'].iloc[i].y))
  
-data_df1_enero_18 = data_df1_enero_18[(data_df1_enero_18['FECHA'] >= '2018-01-01 00:00:00') & (data_df1_enero_18['FECHA'] <= '2018-01-21 23:59:59')]    
+data_df1_enero_18 = data_df1_enero_18[(data_df1_enero_18['FECHA'] >= '2018-01-01 00:00:00') & (data_df1_enero_18['FECHA'] <= '2018-01-31 23:59:59')]    
 
 #data_df1_enero_18 = data_df1_enero_18.iloc[0:3000,:]
 
@@ -89,7 +65,7 @@ start = timeit.default_timer()
 # Importamos el dataframe poligonos.csv
 covs_df = pd.read_csv('poligonos_df_final.csv')
 covs_df = gpd.GeoDataFrame(covs_df, columns=['geometry','Núm de Comando de Atención Inmediata 2020','Rest y Bares'])
-covs_df = covs_df.rename(columns={'Rest y Bares' : 'cov2', 'Núm de Comando de Atención Inmediata 2020' : 'cov1'})
+covs_df = covs_df.rename(columns={'Rest y Bares' : 'cov1', 'Núm de Comando de Atención Inmediata 2020' : 'cov2'})
 # Normalizamos las columnas de covariados
 covs_df['cov1'] = covs_df['cov1']/covs_df['cov1'].sum()
 covs_df['cov2'] = covs_df['cov2']/covs_df['cov2'].sum()
